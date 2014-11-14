0fc8dd1715d8ff7309e74a273766a6df953ce495
jcridge <crij1_12@uni.worc.ac.uk>
2014-11-14 10:32:11 +0000

    Added Plugins

--- www/js/index.js
+++ www/js/index.js
@@ -1,11 +1,16 @@
 $(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
     random();
+	document.getElementById("returnedResult").innerHTML = random();
+	//beep three time
+	navigator.notification.beep(3);
   });            
 });            
 
 function random() {
-	console.log("Running function");
+	//console.log("Running function");
 	return !Math.round(Math.random()); 
-	document.getElementById("returnedResult").innerHTML = Math.random();
 }
+
+
+
